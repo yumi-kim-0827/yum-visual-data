38d4a7b1e1ac1fb701e6c00b4a61eb6b3274d36f
yumi-kim-0827 <yumee0827@naver.com>
2024-11-24 23:49:17 +0900

    feat:메인 내용 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,28 @@
 import React from "react";
-import dynamic from "next/dynamic";
 //components
 import { Fieldset } from "primereact/fieldset";
+import { Button } from "primereact/button";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <Fieldset legend="인포그래픽 만들기">메인</Fieldset>
+    <main className="w-full">
+      <Fieldset legend="인포그래픽 만들기" className="w-full">
+        <h2>
+          데이터를 그래프와 지도에서 시각적으로 확인하고, 더 나은 결정을
+          내려보세요.
+        </h2>
+        <p>간단히 데이터를 입력하고, 직관적인 그래프와 지도에서 그려냅니다.</p>
+        <span>
+          데이터를 시각화하여 더 쉽게 분석하고, 빠르게 통찰을 얻으세요.
+        </span>
+        <Button
+          label="그래프 만들러가기"
+          icon="pi pi-arrow-right"
+          iconPos="right"
+          size="small"
+          rounded
+        />
+      </Fieldset>
     </main>
   );
 }
