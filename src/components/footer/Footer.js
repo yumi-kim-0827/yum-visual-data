68ff8d463a5133b5d466c9270d56fee9ec016138
yumi-kim-0827 <yumee0827@naver.com>
2024-12-08 23:12:29 +0900

    푸터추가

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="p-4 bg-gray-50">
+      <p className="m-0 text-sm">Copyright© 2024 KIM YUMEE</p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,16 +8,18 @@ import { useEffect, useState } from "react";
 //components
 import PanelMenuBar from "@/src/components/sidemenu/PanelMenuBar";
 import Header from "@/src/components/header/Header";
+import Footer from "@/src/components/footer/Footer";
 
 export default function App({ Component, pageProps }) {
   const [theme, setTheme] = useState("dart");
   return (
     <PrimeReactProvider>
       <Header />
-      <div className="p-4 flex gap-2 h-[calc(100vh-87px)]">
+      <div className="p-4 flex gap-2 h-[calc(100vh-155px)]">
         <PanelMenuBar />
         <Component {...pageProps} />
       </div>
+      <Footer />
     </PrimeReactProvider>
   );
 }
