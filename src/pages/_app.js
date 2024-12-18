fc07c55fa50f276ff88aa1a62e473ffa28aa91f2
yumeekim <yumee0827@naver.com>
2024-12-18 10:23:30 +0900

    구글 애널리틱스 스크립트 추가

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,15 +6,28 @@ import "@/src/styles/globals.css";
 import { PrimeReactProvider } from "primereact/api";
 import { useEffect, useState } from "react";
 import { DefaultSeo } from "next-seo";
+import Script from "next/script";
 //components
 import PanelMenuBar from "@/src/components/sidemenu/PanelMenuBar";
 import Header from "@/src/components/header/Header";
 import Footer from "@/src/components/footer/Footer";
 
 export default function App({ Component, pageProps }) {
-  const [theme, setTheme] = useState("dart");
   return (
     <>
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-DCKP3WS77X`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+           window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DCKP3WS77X');
+        `}
+      </Script>
       <DefaultSeo
         title="visual infographic"
         description="로그인 없이 그래프와 차트 등 인포그래픽을 생성하세요, 간단히 데이터를 입력하여 직관적인 그래프와 지도에서 결과를 시각화하세요."
@@ -32,11 +45,11 @@ export default function App({ Component, pageProps }) {
             },
           ],
         }}
-        twitter={{
-          handle: "@twitter_handle",
-          site: "@twitter_handle",
-          cardType: "summary_large_image",
-        }}
+        // twitter={{
+        //   handle: "@twitter_handle",
+        //   site: "@twitter_handle",
+        //   cardType: "summary_large_image",
+        // }}
       />
       <PrimeReactProvider>
         <Header />
